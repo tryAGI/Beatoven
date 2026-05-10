d4d732802d43033e6534e36aa800034efde3e1e3
Konstantin S. <havendv@gmail.com>
2026-05-11 01:40:28 +0400

    feat: Updated OpenAPI spec (#8)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Beatoven/Generated/Beatoven.Models.ComposeTrackPrompt.g.cs
+++ src/libs/Beatoven/Generated/Beatoven.Models.ComposeTrackPrompt.g.cs
@@ -45,5 +45,6 @@ namespace Beatoven
         public ComposeTrackPrompt()
         {
         }
+
     }
 }
--- src/libs/Beatoven/Generated/Beatoven.Models.ComposeTrackResponse.g.cs
+++ src/libs/Beatoven/Generated/Beatoven.Models.ComposeTrackResponse.g.cs
@@ -57,5 +57,6 @@ namespace Beatoven
         public ComposeTrackResponse()
         {
         }
+
     }
 }
